c364419adfc08a720a97da945387a9d9d3482546
zhang hailong <hailongz@qq.com>
2016-10-11 16:10:47 +0800

    update

--- lib/lua/job/http.lua
+++ lib/lua/job/http.lua
@@ -6,6 +6,7 @@ local table = require("table")
 
 return function(uri,data) 
 	ngx.log(ngx.ALERT,'[HTTP] '..conf.baseURL .. uri)
+	ngx.log(ngx.ALERT,'[HTTP] '.. kk.json.encode(data))
 	return kk.http.json(conf.baseURL .. uri,data)
 end
 
