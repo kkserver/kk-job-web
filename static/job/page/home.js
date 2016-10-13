f4a69a42466c37fcd0980e17aa44da7d7800fc64
zhang hailong <hailongz@qq.com>
2016-10-13 10:22:49 +0800

    update

--- static/job/page/home.js
+++ static/job/page/home.js
@@ -1,6 +1,28 @@
 
+
+Page = {}
+
+Page.progress = function(value) {
+	this.style.width = value +"%";
+};
+
+Page.status = function(value) {
+	$(this).removeClass("progress").removeClass("ok").removeClass("fail");
+	if(value == 1) {
+		$(this).addClass("progress");
+	}
+	else if(value == 200) {
+		$(this).addClass("ok");
+	}
+	else if(value == 300 || value == 400) {
+		$(this).addClass("fail");
+	}
+};
+
+
 $(function(){
 
+	view.set("versions",page.versions || []);
 
 	$(document.body).on("click",".ui-list-item",function(e){
 		window.location.hash = $("input[type=hidden][name=hash]",this).val();
