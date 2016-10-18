76f8b2fef30d42ca7ca50db4741ca7b8d6a21162
zhang hailong <hailongz@qq.com>
2016-10-18 14:35:19 +0800

    update

--- static/job/home/add.js
+++ static/job/home/add.js
@@ -5,5 +5,68 @@ kk.page().onview("/add",{
 	container: "#kk-page",
 	onload : function(view,options) {
 
+		var options = [];
+
+		if(page && page.job && page.job.options){
+			var v = JSON.parse(page.job.options);
+			if(typeof v == "object") {
+				for(var key in v) {
+					var option = v[key];
+					if(option.editable) {
+						option["key"] = key;
+						options.push(option)
+					}
+				}
+			}
+		}
+
+		view.set("options",options);
+
+		$("form",view.element).on("submit",function(e){
+
+			var data = {};
+
+			var vs = $(this).serializeArray();
+
+			var options = {};
+
+			for(var i in vs) {
+				data[vs[i].name] = vs[i].value;
+			}
+
+			if(page && page.job && page.job.options){
+				var v = JSON.parse(page.job.options);
+				if(typeof v == "object") {
+					for(var key in v) {
+						var option = v[key];
+						if(option.editable) {
+							if(data[key] !== undefined) {
+								options[key] = data[key];
+							}
+						}
+					}
+				}
+			}
+
+			var disabled = $("input[type=submit]",this).attr("disabled","disabled");
+
+			$.post("/job/api/version/add.json", {jobId:ui.url.queryValue("id"),options:JSON.stringify(options),title: data.title },function(data){
+
+				disabled.removeAttr("disabled");
+
+				if( data && data.errmsg) {
+					alert(data.errmsg);
+				} else if (data && data.version) {
+					window.location = "?id=" + ui.url.queryValue("id") + "#/" + data.version.id;
+				} else {
+					window.location = "?id=" + ui.url.queryValue("id") ;
+				}
+
+				window.location.reload();
+
+			},"json");
+
+			return false;
+		}).removeAttr("onsubmit");
 	}
 });
--- static/job/home/version.js
+++ static/job/home/version.js
@@ -5,5 +5,61 @@ kk.page().onview(/\/[0-9]*/i,{
 	container: "#kk-page",
 	onload : function(view,options) {
 
+		var version = "0"
+
+		this.path.replace(/\/([0-9]*)/i,function(text,v){
+			version = v
+		});
+
+		$.get("/job/api/version/log.json",{ jobId:ui.url.queryValue("id"),version:version,limit: 500},function(data){
+
+			if(data && data.logs) {
+				view.set("logs",data.logs);
+			}
+			else {
+				view.set("logs",[]);
+			}
+
+		},"json");
+
+		$.get("/job/api/version/get.json",{ jobId:ui.url.queryValue("id"),version:version},function(data){
+
+			if(data && data.version) {
+
+				var options = [];
+				var optionsMap = {};
+
+				if(page && page.job && page.job.options){
+					var v = JSON.parse(page.job.options);
+					if(typeof v == "object") {
+						for(var key in v) {
+							var option = v[key];
+							optionsMap[key] = option
+							option["key"] = key;
+							options.push(option)
+						}
+					}
+				}
+
+				if(data && data.version && data.version.options) {
+					var v = JSON.parse(data.version.options);
+					if(typeof v == "object") {
+						for(var key in v) {
+							var option = optionsMap[key];
+							if(option) {
+								option.value = v[key];
+							} 
+						}
+					}
+				}
+
+				view.set("options",options);
+			}
+			else {
+				view.set("options",[]);
+			}
+
+		},"json");
+
 	}
 });
