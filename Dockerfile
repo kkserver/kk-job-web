913bfac8f4fb69252d2f6a46a67bf5d60eb6e98c
zhang hailong <hailongz@qq.com>
2016-10-21 15:44:14 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,9 @@
-FROM registry.cn-hangzhou.aliyuncs.com/kk/kk-web:debug
+FROM registry.cn-hangzhou.aliyuncs.com/kk/kk-web:latest
 
 COPY ./@app/job /@app/job
 
 COPY ./lib/lua/job /lib/lua/job
 
-COPY ./lib/lua/conf /lib/lua/conf
+COPY ./lib/lua/conf/job.lua /lib/lua/conf/job.lua
 
 COPY ./static/job /static/job
-
-VOLUME /@app/job
-VOLUME /static/job
---
+++ build.sh
@@ -0,0 +1,67 @@
+#/bin/sh
+
+TAG=`date +%Y%m%d%H%M%S`
+WORKDIR=`pwd`
+
+
+exitCommand() {
+	exit
+}
+
+runCommand() {
+	echo $CMD
+	$CMD
+	if [ $? -ne 0 ]; then
+		echo -e "[FAIL] $CMD"
+		exitCommand 3
+	fi 
+}
+
+buildProject() {
+
+	#static compressor
+
+	if [ -d "$HOME/.kk-shell" ]; then
+		cd "$HOME/.kk-shell"
+		git pull origin master "$HOME/.kk-shell"
+		cd $WORKDIR
+	else
+		git clone http://github.com/kkserver/kk-shell $HOME/.kk-shell
+		chmod +x $HOME/.kk-shell/web/build.sh
+		chmod +x $HOME/.kk-shell/web/view.py
+	fi
+
+	export DEBUG
+
+	CMD="$HOME/.kk-shell/web/build.sh"
+	runCommand
+
+	#docker
+	CMD="docker build -t $PROJECT:$TAG ."
+	runCommand
+
+	CMD="docker push $PROJECT:$TAG"
+	runCommand
+
+	CMD="docker tag $PROJECT:$TAG $PROJECT:latest"
+	runCommand
+
+	CMD="docker push $PROJECT:latest"
+	runCommand
+
+}
+
+echo $WORKDIR
+echo "DEBUG: $DEBUG"
+
+#go
+
+PROJECT="registry.cn-hangzhou.aliyuncs.com/kk/kk-job-web"
+buildProject
+
+#exit
+
+echo "[OK] TAG: $TAG"
+
+exitCommand
+
